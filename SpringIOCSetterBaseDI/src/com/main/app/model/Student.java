7d595b80fb94012fda31a6dbb1268166a513e5cf
ShrutiFaiz <shruti.faizpurkar@gmail.com>
2023-03-23 09:31:07 +0530

    Student pojo created

---
+++ SpringIOCSetterBaseDI/src/com/main/app/model/Student.java
@@ -0,0 +1,24 @@
+package com.main.app.model;
+
+public class Student {
+	private int rollno;
+	private String name;
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Student(int rollno, String name) {
+		super();
+		this.rollno = rollno;
+		this.name = name;
+	}
+	
+}
