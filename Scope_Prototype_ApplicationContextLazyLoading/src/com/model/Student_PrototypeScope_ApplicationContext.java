46515df5b3a29e75ac21c0ea2cfccafb882896aa
ShrutiFaiz <shruti.faizpurkar@gmail.com>
2023-03-23 09:06:27 +0530

    initial commit

---
+++ Scope_Prototype_ApplicationContextLazyLoading/src/com/model/Student_PrototypeScope_ApplicationContext.java
@@ -0,0 +1,9 @@
+package com.model;
+
+public class Student_PrototypeScope_ApplicationContext {
+	
+	public Student_PrototypeScope_ApplicationContext()
+	{
+		System.out.println("---- Prototype Scope ApplicationContext Constructor Lazy Loading ----");
+	}
+}
---
+++ Scope_Prototype_BeanFactoryLazyLoadig/src/com/client/Test_SingletonScope_BeanFactory.java
@@ -0,0 +1,28 @@
+package com.client;
+
+import org.springframework.beans.factory.BeanFactory;
+
+
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+
+import com.model.Student_SingletonScope_BeanFactory;
+
+public class Test_SingletonScope_BeanFactory
+{
+	public static void main(String[] args)
+	{
+
+		Resource r=new ClassPathResource("beans.xml");
+		BeanFactory bf= new XmlBeanFactory(r);
+		//scope =prototype
+		//When we call getBean method then only constructor will call. Comment getBean method.  
+		//It will not create object. Object will be created only when we call getBean method
+		//This is called Lazy loading 
+		//bf.getBean("s");
+		
+	}
+}
